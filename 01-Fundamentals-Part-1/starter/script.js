e27cac3e7e66a1c3adc1dd916f0cb22f02430ea4
SGEMIN1 <sgemin1@ford.com>
2022-09-26 18:44:31 -0400

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,43 @@
+/*
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+*/
+////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+let markWeight1 = 78;
+let markHeight1 = 1.69;
+let johnWeight1 = 92;
+let johnHeight1 = 1.95;
+
+
+let markWeight2 = 95;
+let markHeight2 = 1.88;
+let johnWeight2 = 85;
+let johnHeight2 = 1.76;
+
+let markHigherBMI;
+
+
+function calculateBmi(mass, height) {
+    return mass / (height ** 2)
+}
+
+markHigherBMI = calculateBmi(markWeight1, markHeight1) > calculateBmi(johnWeight1, johnHeight1)
+console.log("Case 1: " + markHigherBMI)
+markHigherBMI = calculateBmi(markWeight2, markHeight2) > calculateBmi(johnWeight2, johnHeight2)
+console.log("Case 2: " + markHigherBMI)
